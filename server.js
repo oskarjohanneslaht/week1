bd16907e5d6fa00e045762f00971e88f6dfd5b51
Oskar <oskar.johannes.laht@tktk.ee>
2023-11-27 16:07:40 +0200

    week 1 code

---
+++ server.js
@@ -0,0 +1,19 @@
+//console.log("hello world");
+//cntr + s salvestamine
+//cntr + c kui jookseb kokku 
+
+let firstName = 'Harry';
+let lastName = 'Potter';
+
+
+//console.log (firstName);
+//console.log("hello world " + firstName + "!");
+
+
+//template literals
+//console.log(`hello world, ${firstName}!`); 
+
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
+
